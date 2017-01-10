aa02677d624d69e54dbd9c39835106181dca8b27
DakotaHarward <daktoa101@gmail.com>
2017-01-10 14:51:34 -0700

    Fixed object.js

--- js/object.js
+++ js/object.js
@@ -1,36 +1,34 @@
 var maxId = 0;
-//Suggest moving all functions to Thing.prototype
+
 function Thing() {
   this.id = maxId++;
-  this.velocity = 0; //Velocity is a vector, not a number. Should be a THREE.Vector3.
-  this.position = new THREE.Vector3( 0, 0 ,0);
-  this.update = function(){};
-  this.damage = 0; //Suggest replacing with a Hull variable, which will be decreased accordingly every time the object is damaged.
+  this.velocity = new THREE.Vector3( 0, 0, 0 );
+  this.position = new THREE.Vector3( 0, 0, 0 );
+  this.hull = 1000;
 }
 
-//Trevor suggests removing this function.
-function inherit() {
-    return Thing.call(this, Thing.id, Thing.velocity, Thing.position, Thing.update, Thing.damage);
-}
+Thing.prototype.update = function(){};
 
-//Suggest adding Thing.call(this), and removing all references to Inherit.
-//In addition, Ship needs to copy Thing's prototype.
-//After copying the prototype of Thing, Ship needs to replace the prototype.constructor with its own.
 function Ship() {
-  inherit();
-  this.fireProjectile = function(){};
+  Thing.call(this);
 }
-//Suggest removing object.create and replacing with according code.
+
+Ship.prototype = Object.create(Thing.prototype);
+Ship.prototype.fireProjectile = function(){
+    console.log("fired a torpedo at Trevor's face.");
+};
+Ship.prototype.constructor = Ship;
+
 function Projectile(velocity) {
-  object.create(Thing);
-  this.damage = 0;
+  Thing.call(this);
 }
+
+Projectile.prototype = Object.create(Thing.prototype);
+Projectile.prototype.constructor = Projectile;
 
 function Box() {
-    object.create(Thing);
+    Thing.call(this);
 }
-//Current implementation doesn't work.
-//Suggest looking at:
-//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Inheritance
-//Make sure to put all variable declarations in the constructor, and all function declarations in the prototype.
-//Make sure to use call() correctly. Also, make sure to copy over the prototype and replace the constructor accordingly.
+
+Box.prototype = Object.create(Thing.prototype);
+Box.prototype.constructor = Box;
