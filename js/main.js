8c63e8e387e0408c2a23838bfa36a87e90a53a1a
Trevor Jex <jextrevor@gmail.com>
2017-01-09 19:32:55 -0700

    Function prototypes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 var Main = {};
+//exit() - Exits the game.
+Main.exit = function(){
+
+}
+//switchLevel(Level) - Switches to level.
+Main.switchLevel = function(level){
+
+}
+//switchMusic(sound) - Switches the music to sound.
+Main.switchMusic = function(sound){
+
+}
+//playSound(sound) - Plays sound.
+Main.playSound = function(sound){
+
+}
+//pause() - Pauses the game.
+Main.pause = function(){
+
+}
+//play() - Resumes the game.
+Main.play = function(){
+
+}
