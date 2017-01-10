cbbfc0bf17c8390621a6a168f176ae84fd98272d
Trevor Jex <jextrevor@gmail.com>
2017-01-09 19:46:14 -0700

    music work

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 var Main = {};
+Main.music = new Audio();
+Main.loadLevel = function(level){
+
+}
 //exit() - Exits the game.
 Main.exit = function(){
 
@@ -9,11 +13,13 @@ Main.switchLevel = function(level){
 }
 //switchMusic(sound) - Switches the music to sound.
 Main.switchMusic = function(sound){
-
+	this.music.pause();
+	this.music = sound;
+	this.music.play();
 }
 //playSound(sound) - Plays sound.
 Main.playSound = function(sound){
-
+	sound.play();
 }
 //pause() - Pauses the game.
 Main.pause = function(){
@@ -23,3 +29,6 @@ Main.pause = function(){
 Main.play = function(){
 
 }
+document.addEventListener("DOMContentLoaded",function(event){
+
+});
