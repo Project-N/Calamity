9a41cb49b87f98e4fb979c61854b50fb4a43986d
dakotaharward <daktoa101@gmail.com>
2017-01-09 23:28:19 -0700

    Made more changes, not stable yet.

--- js/object.js
+++ js/object.js
@@ -1,17 +1,27 @@
-var maxId;
-function Object() {
-  this.id = function() {
-      if(maxId == null){
-          maxId = 0;
-      } else {
-          maxId = ++1;
-      }
-      return maxId;
-  };
+var maxId = 0;
+
+function Thing() {
+  this.id = maxId++;
   this.velocity = 0;
   this.position = new THREE.Vector3( 0, 0 ,0);
-  this.update = function(){
-      
-  };
+  this.update = function(){};
   this.damage = 0;
+}
+
+function inherit() {
+    return Thing.call(this, Thing.id, Thing.velocity, Thing.position, Thing.update, Thing.damage);
+}
+
+function Ship() {
+  inherit();
+  this.fireProjectile = function(){};
+}
+
+function Projectile(velocity) {
+  object.create(Thing);
+  this.damage = 0;
+}
+
+function Box() {
+    object.create(Thing);
 }
