e45069fcbb3f9f130012df92d6984c1faf818ff7
Trevor Jex <jextrevor@gmail.com>
2017-01-10 19:14:12 -0700

    Added licensing

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -6,6 +6,14 @@ stars.update = function(delta){
 }
 mainmenu.objects.push(stars);
 mainmenu.objectives.push(new Objective());
+mainmenu.showcredits = function(){
+	this.title.style.display = "none";
+	this.credits.style.display = "block";
+}
+mainmenu.hidecredits = function(){
+	this.credits.style.display = "none";
+	this.title.style.display = "block";
+}
 mainmenu.load = function(){
 	var music = new Audio("media/bensound-slowmotion.ogg");
 	music.loop = true;
@@ -13,9 +21,15 @@ mainmenu.load = function(){
 	this.title = document.createElement("div");
     this.title.className = "menu";
     this.title.id = "mainmenutitle";
-    this.title.innerHTML = "CALAMITY<br /><br /><a onclick='pointerlock();Main.switchLevel(levelone);'>Play</a><br /><a onclick='Main.exit()'>Exit</a>"; //TODO: Request pointer lock when PLAY button is clicked. Add JS helper file for pointer locks. https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+    this.title.innerHTML = "CALAMITY<br /><br /><a onclick='pointerlock();Main.switchLevel(levelone);'>Play</a><br /><a onclick='mainmenu.showcredits()'>Credits</a><br /><a onclick='Main.exit()'>Exit</a>"; //TODO: Request pointer lock when PLAY button is clicked. Add JS helper file for pointer locks. https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
     document.body.appendChild(this.title);
+    this.credits = document.createElement("div");
+    this.credits.className = "menu";
+    this.credits.style.display = "none";
+    this.credits.innerHTML = "Credits<br /><div style='font-size:12px'>All works are original except those listed below.<br /><br />Using music from Bensound, licensed under the Creative Commons Attribution license. Using fonts created by Severin Meyer, licensed under the SIL Open Font License.</div><a onclick='mainmenu.hidecredits()'>Back</a>"; //TODO: Request pointer lock when PLAY button is clicked. Add JS helper file for pointer locks. https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+    document.body.appendChild(this.credits);
 }
 mainmenu.unload = function(){
 	document.body.removeChild(this.title);
+	document.body.removeChild(this.credits);
 }
